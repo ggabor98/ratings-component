eba10a315dbf208045629646c419c739243f321b
George Adrian GABOR <georgeadrian.gabor@amadeus.com>
2022-06-02 12:32:02 +0200

    Rating component

--- src/pages/Rating.tsx
+++ src/pages/Rating.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { PreviewRates } from '../components/PreviewRates';
 import { Rates } from '../components/Rates';
 
